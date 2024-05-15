123214b4292e6bda51f7772a7d9f2a3c9caa3eb0
Laiza C. M. Z. Truyts <111503805+LaizaCristina@users.noreply.github.com>
2024-05-15 08:26:19 -0300

    up

---
+++ agenda/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import Cadastro from './pages/cadastro/components/main';
+import EditarUsuario from './pages/editar/components/main';
+import Login from './pages/login/components/main';
+
+function App() {
+  const idUsuario = '3'
+  return (
+    <div>
+      <Cadastro/>
+    </div>
+  );
+}
+
+export default App;
